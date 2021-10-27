401f2bcd016db468b32d39f5f2a72b4728e5307c
Jake Beresford <jake.beresford@palmetto.com>
2021-10-27 10:02:14 -0400

    Provide version in plugin instance
    DATA-353

--- palm/plugins/dbt/dbt_plugin.py
+++ palm/plugins/dbt/dbt_plugin.py
@@ -1,7 +1,17 @@
 from pathlib import Path
 from palm.plugins.base import BasePlugin
+import pkg_resources
+
+def get_version():
+    try:
+        version = pkg_resources.require("palm-dbt")[0].version
+    except pkg_resources.DistributionNotFound:
+        version = 'unknown'
+    return version
+
 
 DbtPlugin = BasePlugin(
     name = 'dbt', 
     command_dir = Path(__file__).parent / 'commands',
+    version = get_version()
 )
