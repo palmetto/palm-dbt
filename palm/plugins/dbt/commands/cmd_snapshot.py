6a57ec363f2395ce44c694b2bce0836abcb4fe35
Mariah Rogers <mariah.rogers@palmetto.com>
2021-11-15 15:00:17 -0800

    Palm snapshot is missing dbt deps #DATA-521

--- palm/plugins/dbt/commands/cmd_snapshot.py
+++ palm/plugins/dbt/commands/cmd_snapshot.py
@@ -1,19 +1,24 @@
 import click
 from typing import Optional
-from palm.plugins.dbt.dbt_palm_utils import dbt_env_vars
+from palm.plugins.dbt.dbt_palm_utils import shell_options, dbt_env_vars
 
 @click.command("snapshot")
 @click.option("--persist", is_flag=True, help="will not drop the test schema at the end")
 @click.option("--select", multiple=True)
+@click.option("--fast", is_flag=True, help="will skip clean/deps/seed")
 @click.pass_context
 def cli(ctx, 
         persist: bool,
+        fast: bool,
         select: Optional[tuple]=()):
     """ Executes the DBT snapshots."""
-    
-    cmd = "dbt snapshot"
+
+    if fast:
+      cmd = "dbt snapshot"
+    else:
+      cmd = "dbt clean && dbt deps && dbt snapshot"
     if select:
-        cmd += f" --select {select}"
+      cmd += f" --select {select}"
     if not persist:
         cmd += " && dbt run-operation drop_branch_schemas" 
 
