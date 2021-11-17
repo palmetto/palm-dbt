85e6a271da1c8336542bf7c95b3c017cc2979867
Emily Ekdahl <emily.ekdahl@palmetto.com>
2021-11-17 11:11:28 -0600

    check for profiles file | DATA-479

--- palm/plugins/dbt/dbt_containerizer.py
+++ palm/plugins/dbt/dbt_containerizer.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 from typing import Optional
 from palm.containerizer import PythonContainerizer
-
+import sys
+from palm.palm_exceptions import AbortPalm
+import click
 
 class DbtContainerizer(PythonContainerizer):
     """
@@ -13,6 +15,37 @@ class DbtContainerizer(PythonContainerizer):
         self.template_dir = template_dir
         self.dbt_version = dbt_version
         self.package_manager = ''
+        self.profiles_file = ''
+    
+    def run(self) -> None:
+        """Runs the containerizer.
+
+        Returns:
+            None
+        """
+        super().check_setup()
+        self.package_manager = super().detect_package_manager()
+        self.profiles_file = self.detect_profiles_file()
+        super().generate(self.target_dir, self.replacements)
+        
+
+    def detect_profiles_file(self) -> str:
+        """Determines whether or not there is a profiles.yml file in the project root.
+
+        Returns:
+            str: exists | unknown
+        """
+        if self.has_profiles_file():
+            return "exists"
+
+        # Unknown profiles.yml, prompt to setup profiles.yml
+        try:
+            self.optionally_add_profiles_file()
+        except AbortPalm:
+            click.secho("Aborting containerization", fg="red")
+            sys.exit(1)
+        return "exists"
+
 
     @property
     def replacements(self) -> dict:
@@ -28,3 +61,32 @@ class DbtContainerizer(PythonContainerizer):
 
     def validate_python_version(self) -> bool:
         return True
+
+    def optionally_add_profiles_file(self):
+        """Optionally, add a profiles.yml file to the project root if it doesn't exist
+
+        Raises:
+            AbortPalm: Abort if user does not want to add profiles.yml
+        """
+        use_profiles_default = click.confirm(
+            "Unable to detect a profiles.yml file, profiles.yml will be used by default. Continue?"
+        )
+        if use_profiles_default:
+            Path("profiles.yml").touch()
+        else:
+            raise AbortPalm("Aborting")
+
+    def has_profiles_file(self) -> bool:
+        """Checks whether or not the project has a profiles.yml file.
+
+        Returns:
+            bool: true if profiles.yml or profiles.yaml exists
+        """
+        profiles_files = ['profiles.yml', 'profiles.yaml']
+
+        for file in profiles_files:
+            if Path(file).exists():
+                breakpoint()
+                return True
+
+        return False
