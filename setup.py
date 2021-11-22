52e8d71c85919265215179ec5e1cff343b6a3541
Jake Beresford <jake.beresford@palmetto.com>
2021-11-22 13:07:41 -0500

    PEP8
    DATA-479

--- setup.py
+++ setup.py
@@ -4,12 +4,15 @@ import sys
 
 # require version of setuptools that supports find_namespace_packages
 from setuptools import setup
+
 try:
     from setuptools import find_namespace_packages
 except ImportError:
     # the user has a downlevel version of setuptools.
     print('Error: palm requires setuptools v40.1.0 or higher.')
-    print('Please upgrade setuptools with "pip install --upgrade setuptools" and try again')
+    print(
+        'Please upgrade setuptools with "pip install --upgrade setuptools" and try again'
+    )
     sys.exit(1)
 
 # User-friendly description from README.md
@@ -27,9 +30,7 @@ setup(
     url='https://github.com/palmetto/palm-dbt',
     packages=find_namespace_packages(include=['palm', 'palm.*']),
     package_data={'': ['palm/plugins/dbt/**/*']},
-    install_requires=[
-		'palm>=2.0.0'
-	],    
+    install_requires=['palmcli>=2.0.0'],
     license='Apache License 2.0',
     classifiers=[
         'Development Status :: 4 - Beta',
