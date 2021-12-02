fe0eab13528dc8c5cfe20959a00aff28569746a3
Jake Beresford <jake.beresford@palmetto.com>
2021-12-02 16:01:01 -0500

    Fix linting

--- palm/plugins/dbt/commands/cmd_containerize.py
+++ palm/plugins/dbt/commands/cmd_containerize.py
@@ -6,9 +6,7 @@ from palm.plugins.dbt.dbt_containerizer import DbtContainerizer
 
 @click.command("containerize")
 @click.option(
-    "--version",
-    multiple=False,
-    help="dbt version to use (e.g. 0.21.0)",
+    "--version", multiple=False, help="dbt version to use (e.g. 0.21.0)",
 )
 @click.pass_context
 def cli(ctx, version: Optional[str]):
