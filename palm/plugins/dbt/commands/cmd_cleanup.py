c5fcfc9938ae27192eeb31ffb5ea36e63c02413f
Jake Beresford <jake.beresford@palmetto.com>
2021-12-07 18:15:31 -0500

    Drop branch schemas before clean to ensure all branches are removed Run dbt deps after dbt clean as we will always need to re-instlal deps at this point

--- palm/plugins/dbt/commands/cmd_cleanup.py
+++ palm/plugins/dbt/commands/cmd_cleanup.py
@@ -9,7 +9,7 @@ from palm.plugins.dbt.dbt_palm_utils import dbt_env_vars
 def cli(ctx):
     """Removes any artifacts from Snowflake related to the current branch."""
 
-    cmd = "dbt clean && dbt run-operation drop_branch_schemas"
+    cmd = "dbt run-operation drop_branch_schemas && dbt clean && dbt deps"
     env_vars = dbt_env_vars(ctx.obj.palm.branch)
     success, msg = ctx.obj.run_in_docker(cmd, env_vars)
     click.secho(msg, fg="green" if success else "red")
