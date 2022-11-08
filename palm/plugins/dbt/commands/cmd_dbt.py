a2eb6760e58f6b93d85f74f198637a8e8d9b2196
Jake Beresford <86241217+jakeberesford-palmetto@users.noreply.github.com>
2022-11-08 13:20:50 -0500

    Update palm/plugins/dbt/commands/cmd_dbt.py
    Co-authored-by: Mariah Rogers <85898387+mariahjrogers@users.noreply.github.com>

--- palm/plugins/dbt/commands/cmd_dbt.py
+++ palm/plugins/dbt/commands/cmd_dbt.py
@@ -64,5 +64,5 @@ def cli(
         cmd.append(" && dbt run-operation drop_branch_schemas")
 
     env_vars = dbt_env_vars(ctx.obj.palm.branch)
-    success, msg = ctx.obj.run_in_docker(cmd, env_vars)
+    success, msg = ctx.obj.run_in_docker(" ".join(cmd), env_vars)
     click.secho(msg, fg="green" if success else "red")
