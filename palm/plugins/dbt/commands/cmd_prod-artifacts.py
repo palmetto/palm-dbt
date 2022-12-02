da80e36b6996ea86c48c767d9f25078863acd5e4
Jake Beresford <jake.beresford@palmetto.com>
2022-12-02 15:51:23 -0500

    Command placeholder for downloading prod artifacts
    This is implemented a no-op command as there are many places artifacts can be stored
    and many nuances to how they should be downloaded.
    
    In the future we may implement a mechanism to
    download from common storage locations (dbt cloud, GCS, S3)
    but we do not currently have bandwidth to do this
    and want to keep other valuable features moving

---
+++ palm/plugins/dbt/commands/cmd_prod-artifacts.py
@@ -0,0 +1,24 @@
+import click
+from palm.plugins.dbt.dbt_palm_utils import dbt_env_vars
+
+
+@click.command("prod-artifacts")
+@click.pass_context
+def cli(ctx):
+    """No-op command: Download your DBT artifacts from production.
+
+    This command is a no-op as the implementation will differ depending on your
+    production environment. You should override this command in your own
+    project by running `palm override --name prod-artifacts` and then
+    implementing the logic to download your DBT artifacts from production.
+    """
+
+    plugin_config = ctx.obj.plugin_config("dbt")
+    artifact_path = plugin_config.dbt_artifacts_prod
+
+    cmd = "echo 'No-op command! Please override this command in your own project by running: palm override --name prod-artifacts'"
+
+    env_vars = dbt_env_vars(ctx.obj.palm.branch)
+    success, msg = ctx.obj.run_in_docker(cmd, env_vars)
+
+    click.secho(msg, fg="green" if success else "red")
