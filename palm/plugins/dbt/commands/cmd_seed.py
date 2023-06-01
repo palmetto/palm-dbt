416be81f8c4c5b026a3063575378b11622a98659
Jake Beresford <jake.beresford@palmetto.com>
2023-06-01 09:03:39 -0400

    Updates from PR review:
    - Add short options for select/exclude
    - Use pass_obj and environment pattern for cleaner code
    
    dp-389

--- palm/plugins/dbt/commands/cmd_seed.py
+++ palm/plugins/dbt/commands/cmd_seed.py
@@ -7,17 +7,17 @@ from palm.plugins.dbt.dbt_palm_utils import dbt_env_vars
 @click.option(
     "--clean", is_flag=True, help="drop the test schema after the run is complete"
 )
-@click.option("--select", multiple=True, help="see dbt docs on select flag")
-@click.option("--exclude", multiple=True, help="see dbt docs on exclude flag")
+@click.option("--select", '-s', multiple=True, help="see dbt docs on select flag")
+@click.option("--exclude", '-e', multiple=True, help="see dbt docs on exclude flag")
 @click.option(
     "--no-full-refresh",
     "-nf",
     is_flag=True,
     help="Insert seeds instead of recreating the table",
 )
-@click.pass_context
+@click.obj
 def cli(
-    ctx,
+    environment,
     clean: bool,
     no_full_refresh: bool,
     select: Optional[Tuple] = tuple(),
@@ -37,6 +37,6 @@ def cli(
     if clean:
         cmd.append('&& dbt run-operation drop_branch_schemas')
 
-    env_vars = dbt_env_vars(ctx.obj.palm.branch)
-    success, msg = ctx.obj.run_in_docker(" ".join(cmd), env_vars)
+    env_vars = dbt_env_vars(environment.palm.branch)
+    success, msg = environment.run_in_docker(" ".join(cmd), env_vars)
     click.secho(msg, fg="green" if success else "red")
